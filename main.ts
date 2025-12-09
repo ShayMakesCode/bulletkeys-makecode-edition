0a8cf66278483c0380c0a709776c0ef6bd75a4ea
Shay <213973@isd622.org>
2025-12-09 09:21:09 -0600

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,43 +1,40 @@
-controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    characterAnimations.setCharacterState(Ranghy, characterAnimations.rule(Predicate.FacingUp))
-})
+namespace SpriteKind {
+    export const Enemy2 = SpriteKind.create()
+    export const Enemy3 = SpriteKind.create()
+    export const Enemy4 = SpriteKind.create()
+    export const Enemy5 = SpriteKind.create()
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile = sprites.createProjectileFromSprite(assets.image`Bullet`, Ranghy, 0, 0)
-    if (characterAnimations.matchesRule(Ranghy, characterAnimations.rule(Predicate.FacingUp))) {
-        projectile.setVelocity(0, -300)
-    } else if (characterAnimations.matchesRule(Ranghy, characterAnimations.rule(Predicate.FacingDown))) {
-        projectile.setVelocity(0, 300)
+    if (characterAnimations.matchesRule(Ranghy, characterAnimations.rule(Predicate.FacingRight))) {
+        projectile.setVelocity(300, 0)
     } else if (characterAnimations.matchesRule(Ranghy, characterAnimations.rule(Predicate.FacingLeft))) {
         projectile.setVelocity(-300, 0)
-    } else if (characterAnimations.matchesRule(Ranghy, characterAnimations.rule(Predicate.FacingRight))) {
-        projectile.setVelocity(300, 0)
     }
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     characterAnimations.setCharacterState(Ranghy, characterAnimations.rule(Predicate.FacingLeft))
 })
+statusbars.onZero(StatusBarKind.Health, function (status) {
+    game.setGameOverMessage(false, "MORTEM.")
+    game.gameOver(false)
+})
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     characterAnimations.setCharacterState(Ranghy, characterAnimations.rule(Predicate.FacingRight))
 })
-controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (characterAnimations.matchesRule(Ranghy, characterAnimations.rule(Predicate.MovingDown, Predicate.FacingDown))) {
-        characterAnimations.loopFrames(
-        Ranghy,
-        assets.animation`RanghyMoveDown`,
-        100,
-        characterAnimations.rule(Predicate.MovingDown, Predicate.FacingDown)
-        )
-    } else if (characterAnimations.matchesRule(Ranghy, characterAnimations.rule(Predicate.FacingDown, Predicate.NotMoving))) {
-        animation.stopAnimation(animation.AnimationTypes.ImageAnimation, Ranghy)
-    }
+controller.A.onEvent(ControllerButtonEvent.Repeated, function () {
+    Ranghy.setVelocity(0, 50)
 })
 let projectile: Sprite = null
 let Ranghy: Sprite = null
 tiles.setCurrentTilemap(tilemap`level1`)
 Ranghy = sprites.create(assets.image`RanghyFront`, SpriteKind.Player)
+controller.moveSprite(Ranghy, 150, 0)
+Ranghy.setPosition(76, 74)
 characterAnimations.setCharacterAnimationsEnabled(Ranghy, true)
 let statusbar = statusbars.create(20, 4, StatusBarKind.Health)
 statusbar.attachToSprite(Ranghy)
-statusbar.setColor(7, 15)
-scene.cameraFollowSprite(Ranghy)
-controller.moveSprite(Ranghy, 100, 100)
+statusbar.setColor(7, 2)
+statusbar.max = 100
+statusbar.value = 100
+statusbar.setStatusBarFlag(StatusBarFlag.SmoothTransition, true)
